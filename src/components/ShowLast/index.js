76256f56e16efd00fed565af554c1394928ae363
robertorsjr <beto.rodriguesds@gmail.com>
2021-03-09 00:59:42 -0300

    Separator on Head

--- src/components/ShowLast/index.js
+++ src/components/ShowLast/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../Button'
+import { Button, Separator } from '../../components'
 import {
   ShowBox,
   ShowTitle,
@@ -11,8 +11,8 @@ function ShowLast({title, issue, subtitle, description, handleClick, location })
   return(
     <ShowBox >
       <ShowTitle >{title}{issue ? ` #${issue}`  : ''}</ShowTitle>  
-      { subtitle && <ShowSubTitle>{subtitle}</ShowSubTitle> }
-      { description && <ShowDescription>{description} </ShowDescription> }
+      { subtitle ? <ShowSubTitle>{subtitle}</ShowSubTitle> : <Separator y={50}/>}
+      { description ? <ShowDescription>{description} </ShowDescription> : <Separator y={50}/>}
       <Button 
         bkcolor={'#FFFFFF'} 
         color={'#0277BD'} 
